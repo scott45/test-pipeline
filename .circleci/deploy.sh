294136de5791d6fe2e660f5433f496eebb5fac57
scotty-b <businge.scott@andela.com>
2017-11-16 14:55:44 +0300

    copy service key

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -41,7 +41,7 @@ generate_service_account() {
     pwd
     cd ./vof-terraform/shared
     touch account.json
-    cp ${SERVICE_ACCOUNT} account.json
+    ${SERVICE_ACCOUNT} >> account.json
     pwd
 }
 
